095f22bb7653d061b584a16b23cdc0bfde9018fc
Robert Parsons <rmpcoding@gmail.com>
2020-09-24 15:42:24 -0700

    Adds important messages to mongoose.js and mongodb.js files, as well as a new global extension called comment divider

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,20 @@
+/* -------------------------------------------------------------------------- */
+/*         This is sheet is for vanilla MongoDB practice purposes only        */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                      This will eventually be deleted                       */
+/* -------------------------------------------------------------------------- */
+
 const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'tasks_db';
+
 
 MongoClient.connect(
     connectionURL,
@@ -13,100 +26,104 @@ MongoClient.connect(
 
         const db = client.db(databaseName);
 
-        // INSERT METHODS FOR MONGODB
-        // ========================================
-        // db.collection('users').insertOne({
-        //   name: 'Robert',
-        //   age: 31
-        // }, (error, result) => {
-        //   if (error) throw error;
+/* -------------------------------------------------------------------------- */
+/*                         INSERT METHODS FOR MONGODB                         */
+/* -------------------------------------------------------------------------- */
+        db.collection('users').insertOne({
+          name: 'Robert',
+          age: 31
+        }, (error, result) => {
+          if (error) throw error;
 
-        //   console.log(result.ops);
-        // })
+          console.log(result.ops);
+        })
 
-        // db.collection('users').insertMany([
-        //   {
-        //     name: 'Andrew',
-        //     age: 26
-        //   }, {
-        //     name: 'Krishna',
-        //     age: 28
-        //   }, {
-        //     name: 'Alyssa',
-        //     age: 29
-        //   }
-        // ], (error, result) => {
-        //   if (error) throw error;
+        db.collection('users').insertMany([
+          {
+            name: 'Andrew',
+            age: 26
+          }, {
+            name: 'Krishna',
+            age: 28
+          }, {
+            name: 'Alyssa',
+            age: 29
+          }
+        ], (error, result) => {
+          if (error) throw error;
 
-        //   console.table(result.ops);
-        // })
+          console.table(result.ops);
+        })
 
-        // FIND METHODS FOR MONGODB
-        // ========================================
-        // db.collection('users').findOne({ _id: new ObjectID("5f6aca0fc420c4185be56b54") }, (error, result) => {
-        //     if (error) throw error;
+/* -------------------------------------------------------------------------- */
+/*                         FIND METHODS FOR MONGODB                           */
+/* -------------------------------------------------------------------------- */
+        db.collection('users').findOne({ _id: new ObjectID("5f6aca0fc420c4185be56b54") }, (error, result) => {
+            if (error) throw error;
 
-        //     console.log(result);
-        // });
+            console.log(result);
+        });
 
-        // db.collection('users').find({ age: 31 }).toArray((error, results) => {
-        //     if (error) throw error;
-        //     console.table(results);
-        // })
+        db.collection('users').find({ age: 31 }).toArray((error, results) => {
+            if (error) throw error;
+            console.table(results);
+        })
 
-        // db.collection('tasks').findOne({ _id: new ObjectID("5f6aacdaf9bf3c1632cf0f18")}, (error, result) => {
-        //   if (error) throw error;
-        //   console.log(result)
-        // })
+        db.collection('tasks').findOne({ _id: new ObjectID("5f6aacdaf9bf3c1632cf0f18")}, (error, result) => {
+          if (error) throw error;
+          console.log(result)
+        })
 
-        // db.collection('tasks').find({ completed: false }).toArray((error, results) => {
-        //   if (error) throw error;
-        //   console.log(results)
-        // })
+        db.collection('tasks').find({ completed: false }).toArray((error, results) => {
+          if (error) throw error;
+          console.log(results)
+        })
 
-        // UPDATE METHODS FOR MONGODB
-        // ========================================
-        // db.collection('users').updateOne({
-        //   _id: new ObjectID("5f6aa9abc6be1a14e3ff7e10")
-        // }, {
-        //   $set: {
-        //     name: 'Oliver'
-        //   }
-        // }).then((result) => {
-        //     console.log(result)
-        // }).catch((error) => {
-        //     console.log(error)
-        // })
+/* -------------------------------------------------------------------------- */
+/*                         UPDATE METHODS FOR MONGODB                         */
+/* -------------------------------------------------------------------------- */
+        db.collection('users').updateOne({
+          _id: new ObjectID("5f6aa9abc6be1a14e3ff7e10")
+        }, {
+          $set: {
+            name: 'Oliver'
+          }
+        }).then((result) => {
+            console.log(result)
+        }).catch((error) => {
+            console.log(error)
+        })
 
-        // db.collection('tasks')
-        //     .updateMany(
-        //         {
-        //             completed: false,
-        //         },
-        //         {
-        //             $set: {
-        //                 completed: true,
-        //             },
-        //         }
-        //     )
-        //     .then((result) => {
-        //         console.log(result);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
+        db.collection('tasks')
+            .updateMany(
+                {
+                    completed: false,
+                },
+                {
+                    $set: {
+                        completed: true,
+                    },
+                }
+            )
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
 
-        // DELETE METHOD FOR MONGODB
-        // ========================================
-        // db.collection('tasks').deleteOne({
-        //     description: "Go out for a run"
-        // }).then((result) => {
-        //     console.log(result);
-        // }).catch((error) => {
-        //     console.log(error);
-        // })
+/* -------------------------------------------------------------------------- */
+/*                         DELETE METHODS FOR MONGODB                         */
+/* -------------------------------------------------------------------------- */
+        db.collection('tasks').deleteOne({
+            description: "Go out for a run"
+        }).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        })
 
-        // END CONNECTION; DO NOT DELETE BELOW THIS
+/* ---------------- END CONNECTION; DO NOT DELETE BELOW THIS ---------------- */
     }
 );
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+const { default: validator } = require('validator');
+const { isEmail, isLength, contains } = require('validator').default;
 
 mongoose.connect('mongodb://127.0.0.1:27017/tasks-api', {
     useNewUrlParser: true,
@@ -7,45 +8,88 @@ mongoose.connect('mongodb://127.0.0.1:27017/tasks-api', {
     useUnifiedTopology: true,
 });
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-    },
-    completed: {
-        type: Boolean,
-    },
-});
+/* -------------------------------------------------------------------------- */
+/*                                  TASKS                                     */
+/* -------------------------------------------------------------------------- */
 
-const newTask = new Task({
-    description: 'Battle Rope Exercises',
-    completed: false,
-});
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false,
+//     },
+// });
 
-newTask
-    .save()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+/* ------------------------------ TASKS Object ------------------------------ */
+
+// const newTask = new Task({
+//     description: 'Just Another Exercise',
+// });
+
+/* ---------------------------- TASKS Connection ---------------------------- */
+
+// newTask
+//     .save()
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+/* -------------------------------------------------------------------------- */
+/*                                  USERS                                     */
+/* -------------------------------------------------------------------------- */
 
 // const User = mongoose.model('User', {
-//   name: {
-//       type: String
-//   },
-//   age: {
-//       type: Number
-//   }
-// })
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     age: {
+//         type: Number,
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         validate(value) {
+//             if (!isEmail(value)) {
+//                 throw new Error('Invalid email');
+//             }
+//         },
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value) {
+//             if (contains(value, 'password', { ignoreCase: true })) {
+//                 throw new Error('Cannot use password as a password');
+//             }
+//         },
+//     },
+// });
+
+/* ------------------------------ USERS Object ------------------------------ */
 
 // const me = new User({
-//   name: 'Robert',
-//   age: 31
-// })
+//     name: 'Robert',
+//     age: 31,
+//     email: 'everyone@wemail.com',
+//     password: 'pAssWord',
+// });
 
-// me.save().then((result) => {
-//     console.log(result)
-// }).catch((error) => {
-//     console.log(error)
-// })
+/* ---------------------------- USERS Connection ---------------------------- */
+
+// me.save()
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
