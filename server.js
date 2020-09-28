f79fa8100e8a6fcfa90c13502e3f288e1c181a09
Robert Parsons <rmpcoding@gmail.com>
2020-09-27 22:19:12 -0700

    Adds router director, separates user router into its own file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const User = require('./models/User');
 const Task = require('./models/Task');
+const router = require('./routers/user')
 require('./db/mongoose');
 
 const app = express();
@@ -10,96 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/* -------------------------------------------------------------------------- */
-/*                                    USERS                                   */
-/* -------------------------------------------------------------------------- */
-
-/* ---------------------------------- POST ---------------------------------- */
-
-app.post('/users', async (req, res) => {
-    console.log(req.body);
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        res.status(201).send(user);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-/* ---------------------------------- READ ---------------------------------- */
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch {
-        res.status(500).send(err);
-    }
-});
-
-/* ---------------------------------- READ ---------------------------------- */
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (err) {
-        res.status(500).send();
-    }
-});
-
-/* --------------------------------- UPDATE --------------------------------- */
-
-app.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    const updatedUser = req.body;
-
-    const updates = Object.keys(updatedUser)
-    const allowedUpdates = ["name", "age", "email", "password"];
-
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(_id, updatedUser, {
-            new: true,
-            runValidators: true,
-        });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user)
-    } catch (err) {
-        res.status(400).send(err)
-    }
-});
-
-/* --------------------------------- DELETE --------------------------------- */
-
-app.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (err) {
-        res.status(404).send(err);
-    }
-});
+app.use(router)
 
 /* -------------------------------------------------------------------------- */
 /*                                    TASKS                                   */
