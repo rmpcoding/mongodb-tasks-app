c22f8ac79d56a0287837011aa3e5c65073913189
Robert Parsons <rmpcoding@gmail.com>
2020-09-27 21:48:08 -0700

    Adds user api routes for reading all/by ID

--- server.js
+++ server.js
@@ -28,6 +28,34 @@ app.post('/users', async (req, res) => {
     }
 });
 
+/* ---------------------------------- READ ---------------------------------- */
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch {
+        res.status(500).send(err);
+    }
+});
+
+/* ---------------------------------- READ ---------------------------------- */
+
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (err) {
+        res.status(500).send();
+    }
+});
+
 /* -------------------------------------------------------------------------- */
 /*                                    TASKS                                   */
 /* -------------------------------------------------------------------------- */
