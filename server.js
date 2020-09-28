0eb944c85826d6b4074d44c20385456f3376ba6e
Robert Parsons <rmpcoding@gmail.com>
2020-09-27 21:30:52 -0700

    Adds working patch route with valid operation check

--- server.js
+++ server.js
@@ -75,7 +75,32 @@ app.get('/tasks/:id', async (req, res) => {
 
 /* --------------------------------- UPDATE --------------------------------- */
 
-app.put('/tasks/:id', (req, res) => {});
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updatedTask = req.body;
+
+    const updates = Object.keys(updatedTask)
+    const allowedUpdates = ["completed", "description"];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, updatedTask, {
+            new: true,
+            runValidators: true,
+        });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+});
 
 /* --------------------------------- DELETE --------------------------------- */
 
