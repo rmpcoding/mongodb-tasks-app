5cef3c755216572f155a60cef828ba5c7be05c1d
Robert Parsons <rmpcoding@gmail.com>
2020-09-30 19:26:40 -0700

    Adds DELETE task-route to delete only under authenticated user

--- routers/task.js
+++ routers/task.js
@@ -97,14 +97,20 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 /* --------------------------------- DELETE --------------------------------- */
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
+    const user = req.user;
 
     try {
-        const task = await Task.findByIdAndDelete(_id);
+        const task = await Task.findOneAndDelete({
+            _id,
+            owner: user._id
+        });
+
         if (!task) {
             return res.status(404).send();
         }
+        
         res.send(task);
     } catch (err) {
         res.status(404).send(err);
